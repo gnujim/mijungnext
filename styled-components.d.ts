c1e2b8d20794ed8987f3720631715c6366ebee71
Mijung Lee <mijung.m@gmail.com>
2021-06-01 13:54:07 -0700

    fix theme typing

--- styled-components.d.ts
+++ styled-components.d.ts
@@ -1,9 +1,22 @@
+// import original module declarations
 import 'styled-components'
-import { DefaultTheme as XStyledDefaultTheme } from '@xstyled/styled-components'
+import '@xstyled/system'
+import {
+  ITheme,
+  DefaultTheme as XStyledDefaultTheme,
+} from '@xstyled/styled-components'
+
+interface AppTheme extends ITheme, XStyledDefaultTheme {
+  /* Customize your theme */
+  fonts: {
+    header: string
+  }
+}
 
 // and extend them!
+declare module '@xstyled/system' {
+  export interface Theme extends AppTheme {}
+}
 declare module 'styled-components' {
-  export interface DefaultTheme extends XStyledDefaultTheme {
-    /* Customize your theme */
-  }
+  export interface DefaultTheme extends AppTheme {}
 }
